fd164b2cccd31166df686a4bd4c645a6ae6633bf
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-12-04 15:11:51 +0200

    solve linear equations in c

---
+++ solve_math_equations/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char alphas[26] = "abcdefghijklmnopqrstuvwxyz";
+
+    char expression[100] = "2x-10=0";
+    char equalSign;
+    int length = strlen(expression);
+    for(int i =0;i<length - 1;i++){
+        if(expression[i] >= 'A' && i = 'z'){
+            equalSign = 
+        }
+    }
+
+}
+
+// 2x - 10 = 0
+// x = 5
